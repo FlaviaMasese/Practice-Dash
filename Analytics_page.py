b570a7ba796ded39a2803f6efedb0353aec05aa5
MrsOye <flaviabosibori@gmail.com>
2022-07-05 23:21:31 +0200

    trial update

--- Analytics_page.py
+++ Analytics_page.py
@@ -30,6 +30,25 @@ LSMS_df=mutate(LSMS1_List,state_name=case_when(f.state==1,'Abia', f.state==2,'Ad
                                         .drop(columns='state'))
 
 
+LSMS2=pd.read_csv(r'C:\Users\flavi\Downloads\data for practice\sect3_plantingw3.csv',low_memory=False)
+LSMS2.rename(columns={'s3q21a': 'Income_dist',},inplace=True)
+LSMS2.dropna(subset=['Income_dist'],inplace=True)
+LSMS2_df=mutate(LSMS2,state_name=case_when(f.state==1,'Abia', f.state==2,'Adamawa',f.state==3,'Akwa Ibom',
+                                                         f.state==4,'Anambra',f.state==5,'Bauchi',f.state==6,'Bayelsa',
+                                                          f.state==7,'Benue',f.state==8,'Borno',f.state==9,'Cross River',
+                                                       f.state==10,'Delta', f.state==11,'Ebonyi',f.state==12,'Edo', 
+                                                        f.state==13,'Ekiti', f.state==14,'Enugu',f.state==15,'Gombe',
+                                                        f.state==16,'Imo',f.state==17,'Jigawa',f.state==18,'Kaduna',
+                                                          f.state==19,'Kano',f.state==20,'Katsina',f.state==21,'Kebbi',
+                                                         f.state==22,'Kogi',f.state==23,'Kwara',f.state==24,'Lagos',
+                                                         f.state==25,'Nasarawa',f.state==26,'Niger',f.state==27,'Ogun',
+                                                         f.state==28,'Ondo',f.state==29,'Osun',f.state==30,'Oyo',
+                                                         f.state==31,'Plateau',f.state==32,'Rivers',f.state==33,'Sokoto',
+                                                        f.state==34,'Taraba',f.state==35,'Yobe',f.state==36,'Zamfara',
+                                                         f.state==37,'FCT Abuja')
+                                        .drop(columns='state'))
+
+
 expend_page = html.Div([
     dbc.Row(
             children=[ 
