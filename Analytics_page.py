b3c478e20a065567c62c497e8e6b140fe306ff82
MrsOye <flaviabosibori@gmail.com>
2022-07-07 01:17:47 +0200

    corrections

--- Analytics_page.py
+++ Analytics_page.py
@@ -29,27 +29,6 @@ LSMS_df=mutate(LSMS1_List,state_name=case_when(f.state==1,'Abia', f.state==2,'Ad
                                                          f.state==37,'FCT Abuja')
                                         .drop(columns='state'))
 
-
-#LSMS2=pd.read_csv(r'C:\Users\flavi\Downloads\data for practice\sect3_plantingw3.csv',low_memory=False)
-#LSMS2.rename(columns={'s3q21a': 'Income_dist',},inplace=True)
-#LSMS2.dropna(subset=['Income_dist'],inplace=True)
-#LSMS2_df=mutate(LSMS2,state_name=case_when(f.state==1,'Abia', f.state==2,'Adamawa',f.state==3,'Akwa Ibom',
-                                                         #f.state==4,'Anambra',f.state==5,'Bauchi',f.state==6,'Bayelsa',
-                                                         # f.state==7,'Benue',f.state==8,'Borno',f.state==9,'Cross River',
-                                                       #f.state==10,'Delta', f.state==11,'Ebonyi',f.state==12,'Edo', 
-                                                       # f.state==13,'Ekiti', f.state==14,'Enugu',f.state==15,'Gombe',
-                                                        #f.state==16,'Imo',f.state==17,'Jigawa',f.state==18,'Kaduna',
-                                                         # f.state==19,'Kano',f.state==20,'Katsina',f.state==21,'Kebbi',
-                                                        # f.state==22,'Kogi',f.state==23,'Kwara',f.state==24,'Lagos',
-                                                         #f.state==25,'Nasarawa',f.state==26,'Niger',f.state==27,'Ogun',
-                                                        # f.state==28,'Ondo',f.state==29,'Osun',f.state==30,'Oyo',
-                                                        # f.state==31,'Plateau',f.state==32,'Rivers',f.state==33,'Sokoto',
-                                                        #f.state==34,'Taraba',f.state==35,'Yobe',f.state==36,'Zamfara',
-                                                        # f.state==37,'FCT Abuja')
-                                       # .drop(columns='state'))
-#LSMS2_df
-
-
 expend_page = html.Div([
     dbc.Row(
             children=[ 
@@ -125,7 +104,7 @@ expend_page = html.Div([
                                                                                             html.H3(id='avg3',
                                                                                                     children='50'
                                                                                                     ),
-                                                                                            html.P('Average Income')
+                                                                                            html.P('Max expenditure')
                                                                                         ]
                                                                                     ),
                                                                                 dbc.Card(
@@ -155,7 +134,7 @@ expend_page = html.Div([
                                                                                             html.H3(id='cred1',
                                                                                                     children='50'
                                                                                                     ),
-                                                                                            html.P('Min Income earning')
+                                                                                            html.P('Average Income earning')
                                                                                         ]
                                                                                     ),
                                                                                 dbc.Card(
@@ -178,7 +157,7 @@ expend_page = html.Div([
                                                                                             html.H3(id='cred2',
                                                                                                     children='50'
                                                                                                     ),
-                                                                                            html.P('Max Income earning')
+                                                                                            html.P('Min Income earning')
                                                                                         ]
                                                                                     ),
                                                                                 dbc.Card(
@@ -194,7 +173,7 @@ expend_page = html.Div([
                                                                         )
                                                                         ),
                                                                     html.Br(),
-                                                                    output_card(card_id='cred3',card_label='Credit 3', 
+                                                                    output_card(card_id='cred3',card_label='Max Income earning', 
                                                                                 icon='bi bi-cash-coin', style={'backgroundColor': 'yellow'}
                                                                                 ),
                                                                    
