0e62abee0ef419ba2efbc21b34dafe150630c021
MrsOye <flaviabosibori@gmail.com>
2022-07-19 22:53:16 +0200

    change

--- app.py
+++ app.py
@@ -236,9 +236,8 @@ def render_state_avg_income(state_selected):
               )
 def render_labour_avg_income(labour_selected):
     state51_df = LSMS2_df[LSMS2_df['labour_type'] == labour_selected] 
-    dff=LSMS2_df.groupby(['labour_type'])['Income_dist'].mean()
-    #state51_Inc_Avg = state51_df['Income_dist'] 
-    return dff, f'{round(state51_df, 2)}'
+    state51_Inc_Avg = state51_df['Income_dist'].min()
+    return f'{round(state51_df, 2)}'
     
 
 
